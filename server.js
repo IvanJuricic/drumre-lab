7cfd81806997b8bebcd1164735bf672da007d20b
IvanJuricic <ivanjuricic97@gmail.com>
2021-12-01 23:53:54 +0100

    Add simple route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => res.send("Server running!\n"));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
