bc2b75f176249e68e08bfac3458de8dcdfc2dac3
IvanJuricic <ivanjuricic97@gmail.com>
2021-12-06 14:55:01 +0100

    Add backend routes and models

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ require("dotenv").config();
 
 const app = express();
 
+// Add JSON middleware
 app.use(express.json({ extended: false }));
+
+// Define routes
+
+app.use("/users", require("./server/routes/users"));
+app.use("/auth", require("./server/routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Server running!\n"));
+//app.get("/", (req, res) => res.send("Server running!\n"));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
